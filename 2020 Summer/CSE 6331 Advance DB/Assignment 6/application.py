c6c19ba508e11ef7d8e8d41fcd102e1ab2a96601
Gauravdeep Singh <26933283+Gauravdeep983@users.noreply.github.com>
2020-08-08 00:57:59 -0500

    ST Homework 4 final, Assignment 6 incept

--- 2020 Summer/CSE 6331 Advance DB/Assignment 6/application.py
+++ 2020 Summer/CSE 6331 Advance DB/Assignment 6/application.py
@@ -1,32 +1,40 @@
-import pyodbc
-import ibm_db
-# import pypyodbc
 import random
 import os
 import string
 from flask import Flask, render_template, url_for, request
+import psycopg2
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-#IBM DB2 connection string
-# conn = ibm_db.connect("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-10.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=zgx10325;PWD=nk^ttsk5lffq6v0h;", "", "")
+# PostgreSQL conn string
+engine = psycopg2.connect(
+    database="awsadb",
+    user="postgres",
+    password="Jeevesh123",
+    host="postgres-adb.cfjuah6u50my.us-east-2.rds.amazonaws.com",
+    port='5432'
+)
+cur = engine.cursor()
 
 #Azure connection string
-server = 'tcp:gaurav-az.database.windows.net'
-database = 'gaurav-adb'
-username = 'gaurav-master'
-password = 'Jeevesh@123'   
-driver= '{ODBC Driver 17 for SQL Server}'
-conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+# server = 'database-1.cfjuah6u50my.us-east-2.rds.amazonaws.com'
+# database = 'aws'
+# username = 'gd_admin'
+# password = 'Hkya3Ug4Cefw2fy'   
+# driver= '{ODBC Driver 17 for SQL Server}'
+# conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
 
 # Home redirect
-@app.route('/')
-def index():    
-    return render_template('main1.html')
+@application.route('/')
+def index():
+    sql = f"SELECT id, latitude, longitude, place FROM l;"
+    cur.execute(sql)
+    arr = cur.fetchall()
+    latitude = arr[0][1]
+    return render_template('main1.html', latitude=latitude)
 
-
-@app.route('/barchart', methods=["POST"])
+@application.route('/barchart', methods=["POST"])
 def barchart():
     start_mag = magnitudefrom = float(request.form.get("mag_from"))
     magnitudeto = float(request.form.get("mag_to"))
@@ -41,15 +49,15 @@ def barchart():
         stmt = ibm_db.exec_immediate(conn, sql)
         data = ibm_db.fetch_tuple(stmt)
         if data:
-            values.append(str(round(start_mag,2))+"-"+str(round(end_mag,2)))
-            values.append(int(data[0]))
-            values.append("%06x" % random.randint(0, 0xFFFFFF))
-            list_of_data.append(values)
+            values.applicationend(str(round(start_mag,2))+"-"+str(round(end_mag,2)))
+            values.applicationend(int(data[0]))
+            values.applicationend("%06x" % random.randint(0, 0xFFFFFF))
+            list_of_data.applicationend(values)
         start_mag = start_mag+step
         loop+=1    
     return render_template('barchart.html', table=list_of_data, increment=step)
 
-@app.route('/scatterchart', methods=["POST", "GET"])
+@application.route('/scatterchart', methods=["POST", "GET"])
 def scatter_chart():  
     pop_min = int(request.form.get("pop_min"))*1000;
     pop_max = int(request.form.get("pop_max"))*1000;
@@ -61,11 +69,11 @@ def scatter_chart():
         total_pop = float(data[1])/1000
         reg_voters = float(data[2])/1000
         tooltip = str(data[0])+'\nTotal pop: '+str(total_pop)+'\nReg. voters: '+ str(reg_voters)
-        arr.append([total_pop, reg_voters, tooltip])
+        arr.applicationend([total_pop, reg_voters, tooltip])
         data = ibm_db.fetch_tuple(stmt)
     return render_template('/scatterchart.html', table=arr, pop_max=pop_max)
 
-@app.route('/linechart', methods=["POST", "GET"])
+@application.route('/linechart', methods=["POST", "GET"])
 def linechart(): 
     pop_min = int(request.form.get("pop_min"))*1000;
     pop_max = int(request.form.get("pop_max"))*1000;
@@ -77,12 +85,11 @@ def linechart():
         total_pop = float(data[1])/1000
         reg_voters = float(data[2])/1000
         # tooltip = str(data[0])+'\nTotal pop: '+str(total_pop)+'\nReg. voters: '+ str(reg_voters)
-        arr.append([total_pop, reg_voters])
+        arr.applicationend([total_pop, reg_voters])
         data = ibm_db.fetch_tuple(stmt)
     return render_template('/linechart.html', data=arr)
 
-
-@app.route('/q6', methods=["POST", "GET"])
+@application.route('/q6', methods=["POST", "GET"])
 def q6(): 
     arr = [['Volcano', 'Elevation']]  
     country = request.form.get("country")
@@ -91,11 +98,10 @@ def q6():
         cursor.execute(sql)
         data = cursor.fetchall()
         for row in data:
-            arr.append([row[0],row[1]])
+            arr.applicationend([row[0],row[1]])
     return render_template('/q6.html', table=arr)
 
-
-@app.route('/q8', methods=["POST", "GET"])
+@application.route('/q8', methods=["POST", "GET"])
 def q8(): 
     volcano_min = request.form.get("volcano_min")
     volcano_max = request.form.get("volcano_max")
@@ -106,12 +112,12 @@ def q8():
         cursor.execute(sql)
         data = cursor.fetchall()
         for row in data:
-            arr.append([row[0],row[1]])
+            arr.applicationend([row[0],row[1]])
     return render_template('/q6.html', table=arr)
 
 cf_port = os.getenv("PORT")
 if __name__ == '__main__':
     if cf_port is None:
-        app.run(debug=True)
+        application.run(debug=True)
     else:
-        app.run(host='0.0.0.0', port=int(cf_port), debug=True)
+        application.run(host='0.0.0.0', port=int(cf_port), debug=True)
